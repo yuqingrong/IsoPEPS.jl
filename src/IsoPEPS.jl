b50cafa7fb4f4b6c3126b4b669ddc5b6c56fc3bc
yuqingrong <rongyuqing01@163.com>
2025-06-29 15:42:24 +0800

    develop (#2)
    * up
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * add poster to typst
    
    * update poster
    
    * up
    
    * up
    
    * update
    
    * up
    
    * update
    
    * peps update
    
    * update
    
    * new peps
    
    * update
    
    * update
    
    * isopeps and mose move
    
    * update
    
    * up
    
    * up
    
    * up
    
    * up
    
    * periodicpeps and isometric_peps_to_unitay
    
    * torus_circuit
    
    * torus_circuit_converge
    
    * update
    
    * clean up: round 1
    
    * clean up pdfs
    
    * clean up: round 2
    
    ---------
    
    Co-authored-by: 容雨晴 <rongyuqing@rongyuqingdeMacBook-Air.local>
    Co-authored-by: yuqingrong <rongyuqing01@163,com>
    Co-authored-by: GiggleLiu <cacate0129@gmail.com>

---
+++ src/IsoPEPS.jl
@@ -0,0 +1,62 @@
+module IsoPEPS
+
+using Random
+using Yao, Yao.EasyBuild
+using KrylovKit: eigsolve
+using LinearAlgebra
+
+using LinearAlgebra
+using Arpack
+using OMEinsum
+import Yao
+import Yao: mat
+using Optim
+import Optim: optimize
+using FiniteDiff, FiniteDifferences
+using SparseArrays,Arpack
+using Graphs, GraphPlot
+using Graphs: SimpleEdge
+using DifferentiationInterface
+using ChainRulesCore
+using Statistics
+using OMEinsumContractionOrders
+using Manifolds, Manopt
+using RecursiveArrayTools
+
+export statevec, vec
+export ising_hamiltonian, ising_hamiltonian_2d,ed_groundstate
+export itime_groundstate!, lanczos
+export transverse_ising,itime_groundstate!
+#export dagger_mps,inner_product
+export MPS,generate_mps,code_dot,vec2mps,code_mps2vec,mps_variation, MPO,transverse_ising_mpo,mat2mpo,local_X,mps_dot_mpo,code_sandwich
+export PEPS, _optimized_code, inner_product, zero_peps, rand_peps, SimpleGraph, SimpleDiGraph, grid, edges,add_edge!, TreeSA, MergeGreedy, generate_peps, 
+       apply_onsite!, getvlabel, getphysicallabel, newlabel, single_sandwich_code, single_sandwich, nflavor, D, two_sandwich_code, two_sandwich,
+       variables, load_variables!, f1, g1!, peps_optimize1, f2, g2!, peps_optimize2, f_ising, g_ising!, peps_optimize_ising, put, mat, 
+       long_range_coherence_peps, cached_peps_optimize1, optimized_peps_optimize2, dtorus, dgrid
+export AutoMooncake, prepare_gradient, gradient
+export local_h,peps_variation,f,g!
+export MPO,transverse_ising_mpo,mat2mpo,local_X
+export truncated_svd,mps_dot_mpo,code_sandwich
+export ishermitian
+export sparse
+export grad, central_fdm
+export dot
+export IsometricPEPS, rand_isometricpeps, mose_move_right!,mose_move_right_step!,peps_fidelity, isometric_peps
+export peps2ugate, get_circuit,get_reuse_circuit,get_iter_circuit,Measure, collect_blocks,I, gensample, long_range_coherence, zz_correlation, mean, Sz_convergence,
+       iter_sz_convergence, extract_sz_measurements, init_random_vq, torus_long_range_coherence, torus_gensample
+export ProductManifold, Stiefel, isopeps_optimize_ising, vector2point, point2vector, isometric_peps_to_unitary
+
+export check_all_sites_convergence, adaptive_all_sites_convergence, monitor_all_sites_iteration_convergence
+export check_convergence_all_sites
+
+include("LanczosAlgorithm.jl")
+include("KrylovkitYao.jl")
+include("ImTebd.jl")
+#include("inner_product_mps.jl")
+include("mps.jl")
+include("mpo.jl")
+include("mpsandmpo.jl")
+include("peps.jl")
+include("isometricpeps.jl")
+include("isopeps2circuit.jl")
+end
